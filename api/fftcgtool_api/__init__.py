cc1150b69542cd04cb00ed8eff0ebb1fcb15814b
LDericher <40151420+ldericher@users.noreply.github.com>
2021-09-09 16:57:09 +0200

    Flask CORS

--- api/fftcgtool_api/__init__.py
+++ api/fftcgtool_api/__init__.py
@@ -2,12 +2,19 @@ import os
 
 import fftcgtool
 from flask import Flask
+from flask_cors import CORS
 
 from .ffdecks import bp as ffdecks_bp
 
 
 def create_app() -> Flask:
     app: Flask = Flask(__name__)
+
+    if app.debug:
+        # Allow CORS in debug mode
+        # production mode will have a reverse proxy
+        CORS(app)
+
     app.register_blueprint(ffdecks_bp)
 
     fftcgtool.RWCardDB(os.path.join(app.root_path, "carddb.zip"))
