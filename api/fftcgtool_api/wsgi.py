f5bd0baff96b8f18163fee167a0c9bdf4837d84d
LDericher <40151420+ldericher@users.noreply.github.com>
2021-09-10 01:08:22 +0200

    Production ready API container

---
+++ api/fftcgtool_api/wsgi.py
@@ -0,0 +1,3 @@
+from fftcgtool_api import create_app
+
+app = create_app()
--- api/setup.py
+++ api/setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="fftcgtool-api",
+    name="fftcgtool_api",
     version="0.1",
     packages=find_packages(),
     author="LDericher",
     author_email="ldericher@gmx.de",
     setup_requires="setuptools-pipfile",
     data_files=[
-        ("", ["fftcgtool-api/carddb.zip"])
+        ("", ["fftcgtool_api/carddb.zip"])
     ],
     include_package_data=True,
     use_pipfile=True,
