4130639610939fe9c2904d1cdee7feedae6fe3df
LDericher <40151420+ldericher@users.noreply.github.com>
2021-09-14 16:34:29 +0200

    logging config

--- api/ttsimport/config.py
+++ api/ttsimport/config.py
@@ -1,7 +1,7 @@
 from functools import lru_cache
 from typing import Optional
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, BaseModel
 
 
 class Settings(BaseSettings):
@@ -17,3 +17,41 @@ def get_settings() -> Settings:
 
 
 settings = get_settings()
+
+
+class LogConfig(BaseModel):
+    """Logging configuration to be set for the server"""
+
+    LOGGER_NAME: str = "mycoolapp"
+    LOG_FORMAT: str = "%(levelname)s in %(name)s (#%(process)d): %(message)s"
+    LOG_LEVEL: str = "DEBUG" if not settings.production_mode else "INFO"
+
+    # Logging config
+    version = 1
+    disable_existing_loggers = False
+    formatters = {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": LOG_FORMAT,
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    }
+    handlers = {
+        "default": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
+    }
+    loggers = {
+        "ttsimport": {"handlers": ["default"], "level": LOG_LEVEL},
+    }
+
+
+@lru_cache
+def get_log_config() -> LogConfig:
+    return LogConfig()
+
+
+log_config = get_log_config()
+
--- api/ttsimport/main.py
+++ api/ttsimport/main.py
@@ -1,45 +1,39 @@
+import logging
 import os
+from logging.config import dictConfig
 
 import fftcgtool
 import uvicorn
-import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from ttsimport.config import settings
+from .config import settings, log_config
 from .ffdecks import router as ffdecks_router
 
+dictConfig(log_config.dict())
 
-def main() -> FastAPI:
-    logging.basicConfig(
-        level=logging.DEBUG if not settings.production_mode else logging.INFO,
-        format="%(levelname)s in %(name)s (#%(process)d): %(message)s",
-    )
+# create app
+app = FastAPI(
+    title="TTSImport API",
+    description="This API provides `fftcgtool` over HTTP.",
+    version="0.1",
+    contact={
+        "name": "LDericher",
+        "email": "LDericher@GMX.de",
+    },
+    license_info={
+        "name": "GPLv3",
+        "url": "https://www.gnu.org/licenses/gpl-3.0.en.html",
+    },
+    openapi_url=settings.openapi_url,
+    docs_url=settings.docs_url if not settings.production_mode else None,
+    redoc_url=settings.redoc_url if not settings.production_mode else None,
+)
+app.include_router(ffdecks_router)
 
-    logger = logging.getLogger(__name__)
-    logger.info(settings)
 
-    here = os.path.dirname(os.path.realpath(__file__))
-
-    # create app
-    app = FastAPI(
-        title="TTSImport API",
-        description="This API provides `fftcgtool` over HTTP.",
-        version="0.1",
-        contact={
-            "name": "LDericher",
-            "email": "LDericher@GMX.de",
-        },
-        license_info={
-            "name": "GPLv3",
-            "url": "https://www.gnu.org/licenses/gpl-3.0.en.html",
-        },
-        openapi_url=settings.openapi_url,
-        docs_url=settings.docs_url if not settings.production_mode else None,
-        redoc_url=settings.redoc_url if not settings.production_mode else None,
-    )
-    app.include_router(ffdecks_router)
-
+@app.on_event("startup")
+def main():
     # Allow CORS in debug mode
     # production mode will have a reverse proxy
     if not settings.production_mode:
@@ -54,7 +48,11 @@ def main() -> FastAPI:
             allow_headers=["*"],
         )
 
+    logger = logging.getLogger(__name__)
+    logger.info(f"{settings = }")
+
     # card db
+    here = os.path.dirname(os.path.realpath(__file__))
     fftcgtool.CardDB(os.path.join(here, "carddb.zip"))
 
     return app
@@ -62,9 +60,8 @@ def main() -> FastAPI:
 
 if __name__ == "__main__":
     uvicorn.run(
-        "ttsimport.main:main",
+        "ttsimport.main:app",
         host="0.0.0.0",
         port=8000,
         reload=True,
-        factory=True,
     )
